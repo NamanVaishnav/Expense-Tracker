a28d542eb61dd0fd040bcc4ca710aeab8cc5dd6b
Naman Vaishnav <namanmvaishnav@gmail.com>
2023-12-29 17:58:39 +0530

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Naman Vaishnav on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
